d94aa8fb04637d1444af10376cc4139e32b0dc14
julioalberto.aranis.contractor@bbva.com <julioalberto.aranis.contractor@bbva.com>
2022-05-08 09:37:28 +0200

    chore: remove useless instructions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,8 @@
 module.exports = {
   plugins: ['@typescript-eslint'],
-  // Prerequisite `eslint-plugin-vue`, being extended, sets
-  // root property `parser` to `'vue-eslint-parser'`, which, for code parsing,
-  // in turn delegates to the parser, specified in `parserOptions.parser`:
-  // https://github.com/vuejs/eslint-plugin-vue#what-is-the-use-the-latest-vue-eslint-parser-error
   parserOptions: {
     parser: require.resolve('@typescript-eslint/parser'),
     extraFileExtensions: ['.vue'],
-    ecmaFeatures: {
-      jsx: true
-    },
     ecmaVersion: 7,
     sourceType: "module"    
   },
@@ -18,7 +11,7 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended'
   ],
   overrides: [{
-    files: ['*.ts', '*.tsx', '*.scss'],
+    files: ['*.ts', '*.scss'],
     rules: {
       // The core 'no-unused-vars' rules (in the eslint:recommeded ruleset)
       // does not work with type definitions
